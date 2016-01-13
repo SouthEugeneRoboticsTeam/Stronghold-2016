a650f5d5cc50118bbf31d92e6b3361535023f51e
Andrew Dassonville <dassonville.andrew@gmail.com>
2016-01-12 20:18:42 -0800

    Remove chooser

--- src/org/usfirst/frc/team2521/robot/Robot.java
+++ src/org/usfirst/frc/team2521/robot/Robot.java
@@ -32,7 +32,7 @@ public class Robot extends IterativeRobot {
 	public void robotInit() {
 		oi = new OI();
 		drivetrain = new Drivetrain();
-		chooser = new SendableChooser();
+		// chooser = new SendableChooser();
 		// chooser.addDefault("Default Auto", new ExampleCommand());
 		// chooser.addObject("My Auto", new MyAutoCommand());
 		SmartDashboard.putData("Auto mode", chooser);
