6feb1abd7e5d44a5eb8895280c5c7d1ab598b51f
srahman23 <shahriarrahman1999@gmail.com>
2016-01-14 06:05:06 -0800

    Initial intake
    Bare bones subsystem.

---
+++ src/org/usfirst/frc/team2521/robot/subsystems/Intake.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team2521.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Intake extends Subsystem {
+	/** 
+	 * Test
+	 */
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
+}
